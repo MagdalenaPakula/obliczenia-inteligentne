4d1f10ad1eb6b6e4aa623b06d43b044dceb142b5
MagdalenaPakula <magdalena.pakula001@gmail.com>
2024-04-10 21:55:50 +0200

    created a single plot_decision_boundary function in visualizations

--- project_1/visualization.py
+++ project_1/visualization.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy import ndarray
 from scipy.spatial import Voronoi
-from sklearn.neighbors import KNeighborsClassifier
 
 
 def plot_voronoi_diagram(
