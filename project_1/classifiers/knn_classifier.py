8a0706eb06213249c381b39033b2a431dec12969
MagdalenaPakula <magdalena.pakula001@gmail.com>
2024-04-11 09:04:05 +0200

    added mlp and svm. SVM works and MLP - to check

--- project_1/classifiers/knn_classifier.py
+++ project_1/classifiers/knn_classifier.py
@@ -9,10 +9,10 @@ from project_1.data import load_generated_datasets
 from project_1.visualization import plot_voronoi_diagram
 
 
-def knn_experiment(dataset, name):
+def knn_experiment(X, dataset_name):
     # Splitting the dataset into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(dataset[:, :-1],
-                                                        dataset[:, -1],
+    X_train, X_test, y_train, y_test = train_test_split(X[:, :-1],
+                                                        X[:, -1],
                                                         train_size=0.8,
                                                         test_size=0.2,
                                                         random_state=42)
@@ -34,7 +34,7 @@ def knn_experiment(dataset, name):
     plt.xlabel("Wartość parametru n_neighbours")
     plt.ylabel("Dokładność")
     plt.legend()
-    plt.title(f"Eksperyment dla zbioru danych: {name}")
+    plt.title(f"Eksperyment dla zbioru danych: {dataset_name}")
     plt.show()
 
     n_neighbors_min = 1
@@ -52,21 +52,22 @@ def knn_experiment(dataset, name):
     knn_max.fit(X_train, y_train)
 
     # Wizualizacja granicy decyzyjnej
-    for name, knn, X, y in [("Minimalny klasyfikator KNN - Zbiór treningowy", knn_min, X_train, y_train),
+    for dataset_name, knn, X, y in [("Minimalny klasyfikator KNN - Zbiór treningowy", knn_min, X_train, y_train),
                             ("Minimalny klasyfikator KNN - Zbiór testowy", knn_min, X_test, y_test),
                             ("Najlepszy klasyfikator KNN - Zbiór treningowy", knn_best, X_train, y_train),
                             ("Najlepszy klasyfikator KNN - Zbiór testowy", knn_best, X_test, y_test),
                             ("Maksymalny klasyfikator KNN - Zbiór treningowy", knn_max, X_train, y_train),
                             ("Maksymalny klasyfikator KNN - Zbiór testowy", knn_max, X_test, y_test)]:
         # Wizualizacja granicy decyzyjnej
-        plot_voronoi_diagram(X, knn.predict(X), y, name)
+        plot_voronoi_diagram(X[:, :2], knn.predict(X[:, :2]), X[:, -1], dataset_name)
+        # plot_voronoi_diagram(X, knn.predict(X), y, name)
 
         # Dodatkowo stworz kod aby przy każdej wizualizacji należy pokazać jak wygląda macierz pomyłek.
         # Obliczenie i wyświetlenie macierzy pomyłek
         cm = confusion_matrix(y, knn.predict(X))
-        plt.figure(figsize=(6, 4))
+        plt.figure()
         sns.heatmap(cm, annot=True, fmt="d")
-        plt.title(f"Macierz pomyłek dla {name}")
+        plt.title(f"Macierz pomyłek dla {dataset_name}")
         plt.xlabel("Przewidziane etykiety")
         plt.ylabel("Rzeczywiste etykiety")
         plt.show()
@@ -82,5 +83,8 @@ if __name__ == "__main__":
     datasets = load_generated_datasets()
 
     # Przeprowadzenie eksperymentu 2_3 (2_1 i 2_2 trzeba poprawic)
-    for dataset, name in datasets[5:6]:
-        knn_experiment(dataset, name)
+    for X, dataset_name in datasets[5:6]:
+        knn_experiment(X, dataset_name)
+
+
+
