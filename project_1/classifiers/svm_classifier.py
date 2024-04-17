6d9f102acf9891fcab9e872a6c6b1cd79b9fb882
MagdalenaPakula <magdalena.pakula001@gmail.com>
2024-04-17 20:30:49 +0200

    added images for strona 5 i 6 and code for SVN

--- project_1/classifiers/svm_classifier.py
+++ project_1/classifiers/svm_classifier.py
@@ -7,47 +7,74 @@ from sklearn.svm import SVC
 
 from project_1.classifiers.mlp_classifier import mlp_experiment
 from project_1.data import load_generated_datasets
-from project_1.visualization import plot_voronoi_diagram
+from project_1.visualization import plot_voronoi_diagram, plot_decision_boundary
 
 
-def svm_experiment(X, dataset_name):
+def svm_experiment(X, plot_title, train_size=0.2):
+    # Splitting the dataset into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X[:, :-1],
                                                         X[:, -1],
-                                                        train_size=0.8,
+                                                        train_size=train_size,
                                                         test_size=0.2,
                                                         random_state=42)
 
-    kernels = ['linear', 'rbf']  # Different kernel parameters
-    best_svm = None
-    best_svm_accuracy = 0
+    C_values = np.logspace(1, 4, 10)  # Exponential range from 31.62 to 1000
 
-    for kernel in kernels:
-        svm = SVC(kernel=kernel)
+    accuracy_train = []
+    accuracy_test = []
+    classifiers = []
+
+    for C in C_values:
+        svm = SVC(kernel='rbf', C=C)
         svm.fit(X_train, y_train)
 
-        accuracy_train = svm.score(X_train, y_train)
-        if accuracy_train > best_svm_accuracy:
-            best_svm_accuracy = accuracy_train
-            best_svm = svm
+        accuracy_train.append(svm.score(X_train, y_train))
+        accuracy_test.append(svm.score(X_test, y_test))
+        classifiers.append(svm)
 
-        # Visualize decision boundary
-        plot_voronoi_diagram(X_train[:, :2], svm.predict(X_train[:, :2]), y_train, f"SVM ({kernel}) - {dataset_name}")
+    # Plotting change in accuracy for different C values
+    plt.plot(C_values, accuracy_train, label="Zbiór treningowy")
+    plt.plot(C_values, accuracy_test, label="Zbiór testowy")
+    plt.xlabel("Wartość parametru C")
+    plt.ylabel("Dokładność")
+    plt.xscale('log')  # Logarithmic scale for C values
+    plt.legend()
+    plt.title(f"Eksperyment dla zbioru danych: {plot_title}")
+    plt.show()
 
-        # Confusion matrix
-        cm = confusion_matrix(y_train, svm.predict(X_train))
+    best_C_index = np.argmax(accuracy_test)
+    best_C = C_values[best_C_index]
+
+    svm_min = classifiers[0]
+    svm_best = classifiers[best_C_index]
+    svm_max = classifiers[-1]
+
+    svm_min.fit(X_train, y_train)
+    svm_best.fit(X_train, y_train)
+    svm_max.fit(X_train, y_train)
+
+    # Decision boundary and confusion matrix visualization
+    for plot_title, svm, X, y in [("Minimalny klasyfikator SVM - Zbiór treningowy", svm_min, X_train, y_train),
+                                  ("Minimalny klasyfikator SVM - Zbiór testowy", svm_min, X_test, y_test),
+                                  ("Najlepszy klasyfikator SVM - Zbiór treningowy", svm_best, X_train, y_train),
+                                  ("Najlepszy klasyfikator SVM - Zbiór testowy", svm_best, X_test, y_test),
+                                  ("Maksymalny klasyfikator SVM - Zbiór treningowy", svm_max, X_train, y_train),
+                                  ("Maksymalny klasyfikator SVM - Zbiór testowy", svm_max, X_test, y_test)]:
+
+        plot_decision_boundary(lambda x: svm.predict(x), X, y, plot_title, resolution=200)
+
+
+        cm = confusion_matrix(y, svm.predict(X))
         plt.figure()
         sns.heatmap(cm, annot=True, fmt="d")
-        plt.title(f"Confusion Matrix for SVM ({kernel}) on {dataset_name}")
-        plt.xlabel("Predicted labels")
-        plt.ylabel("True labels")
+        plt.title(f"Macierz pomyłek dla {plot_title}")
+        plt.xlabel("Przewidziane etykiety")
+        plt.ylabel("Rzeczywiste etykiety")
         plt.show()
-
-    return best_svm
 
 
 if __name__ == "__main__":
     datasets = load_generated_datasets()
 
     for X, dataset_name in datasets[5:6]:
-        best_svm = svm_experiment(X, dataset_name)
-        best_mlp = mlp_experiment(X, dataset_name)
+       svm_experiment(X, dataset_name)
