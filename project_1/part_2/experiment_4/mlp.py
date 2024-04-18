6d8686a8c50afaf410e53f98040e62d194b9ce6e
Jakub Pawlak <kuba_pawlak@icloud.com>
2024-04-18 20:14:50 +0200

    Create experiment 4

--- project_1/part_2/experiment_4/mlp.py
+++ project_1/part_2/experiment_4/mlp.py
@@ -1,5 +1,7 @@
+import pathlib
 import warnings
-from typing import List
+from copy import deepcopy
+from typing import Dict
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -11,78 +13,138 @@ from project_1.visualization import plot_decision_boundary
 
 warnings.filterwarnings("ignore")
 
+NUM_EPOCHS = 100_000
 
-def experiment_4(dataset, dataset_name, train_size):
+
+def calculate_score(train_score, test_score):
+    return test_score + 1e-5 * train_score
+
+
+def visualize_training_over_time(dataset, train_size):
     X_train, X_test, y_train, y_test = train_test_split(dataset[:, :-1],
                                                         dataset[:, -1],
                                                         train_size=train_size,
                                                         test_size=0.2,
                                                         random_state=42)
 
-    layer_sizes = range(1, 10)
-    classifiers: List[MLPClassifier] = []
+    all_classes = np.unique(dataset[:, -1])
+    epochs = range(1, NUM_EPOCHS + 1)
     train_scores = []
     test_scores = []
 
-    for num_neurons in layer_sizes:
-        classifier = MLPClassifier(hidden_layer_sizes=(5,),  # changed to the most optimal num_neurons from exp 2 and 3
-                                   activation='relu',
-                                   max_iter=100000,
-                                   tol=0,
-                                   n_iter_no_change=100000,
-                                   solver='sgd',
-                                   random_state=42)
+    classifier = MLPClassifier(hidden_layer_sizes=(5,),  # changed to the most optimal num_neurons from exp 2 and 3
+                               activation='relu',
+                               max_iter=NUM_EPOCHS,
+                               tol=0,
+                               n_iter_no_change=100000,
+                               solver='sgd',
+                               random_state=42)
 
-        train_acc = []
-        test_acc = []
-        print(f'Training network with {num_neurons} neurons on dataset {dataset_name}...')
-        for _ in range(1000):  # TO CHANGE LATER TO 100_000 (according to wikamp)
-            classifier.partial_fit(X_train, y_train, classes=np.unique(y_train))
-            train_acc.append(classifier.score(X_train, y_train))
-            test_acc.append(classifier.score(X_test, y_test))
+    classifiers: Dict[str, MLPClassifier] = dict()
 
-        train_scores.append(train_acc)
-        test_scores.append(test_acc)
-        classifiers.append(classifier)
+    for epoch in epochs:
+        if epoch % 1_000 == 0:
+            print(f'Epoch {epoch}')
 
-    # Plot accuracy changes on training and test sets for subsequent epochs
-    plt.figure(figsize=(10, 6))
-    plt.plot(range(1, 1001), train_scores[0], label='Training')
-    plt.plot(range(1, 1001), test_scores[0], label='Test')
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
-    plt.title(f'Accuracy Changes on Training and Test Sets for MLP on Dataset {dataset_name} for train size = {train_size}')
-    plt.legend()
-    plt.grid(True)
+        classifier.partial_fit(X_train, y_train, classes=all_classes)
+
+        train_score = classifier.score(X_train, y_train)
+        test_score = classifier.score(X_test, y_test)
+
+        if epoch == epochs[0]:
+            classifiers['first'] = deepcopy(classifier)
+        else:
+            # can only calculate best score if there are entries
+            best_score = np.max([calculate_score(tr, tst) for (tr, tst) in zip(train_scores, test_scores)])
+            if calculate_score(train_score, test_score) > best_score:
+                classifiers['best'] = deepcopy(classifier)
+        if epoch == epochs[-1]:
+            classifiers['last'] = deepcopy(classifier)
+
+        train_scores.append(train_score)
+        test_scores.append(test_score)
+
+    # Plot accuracy over time
+    plt.plot(epochs, train_scores, label='train set')
+    plt.plot(epochs, test_scores, label='test set')
+    plt.grid(linestyle='--', axis='x')
+    plt.legend(loc='best')
+    plt.title(f'MLP accuracy over time, for training set size of {train_size}')
+    pathlib.Path(f'exp_4/set_{train_size}').mkdir(parents=True, exist_ok=True)
+    plt.savefig(f'exp_4/set_{train_size}/accuracies.png')
     plt.show()
     plt.close()
 
-    # Find best classifier based on test set
-    best_classifier_idx = np.argmax([max(scores) for scores in test_scores])
-    best_classifier = classifiers[best_classifier_idx]
+    for case, classifier in classifiers.items():
+        pathlib.Path(f'exp_4/set_{train_size}/{case}').mkdir(parents=True, exist_ok=True)
 
-    # Plot decision boundaries for different epochs
-    epochs_to_visualize = [0, np.argmax(test_scores[best_classifier_idx]), 999]
-    for epoch in epochs_to_visualize:
-        plt.figure(figsize=(12, 5))
-        for idx, (name, X, y) in enumerate([('Training', X_train, y_train), ('Test', X_test, y_test)]):
-            plt.subplot(1, 2, idx + 1)
-            # Retrain the classifier for the specified number of epochs
-            classifier_at_epoch = MLPClassifier(hidden_layer_sizes=(5,),  # Set the same configuration
-                                                 activation='relu',
-                                                 max_iter=epoch+1,  # Train for epoch + 1 iterations
-                                                 tol=0,
-                                                 solver='sgd',
-                                                 random_state=42)
-            classifier_at_epoch.fit(X_train, y_train)
-            plot_decision_boundary(lambda x: classifier_at_epoch.predict(x), X, y,
-                                   title=f'Decision Boundary on {name} Set - Epoch {epoch}')
-        plt.tight_layout()
+        plot_decision_boundary(lambda x: classifier.predict(x),
+                               X_train, y_train, title=f'Decision boundary for {case} epoch on train set')
+        plt.savefig(f'exp_4/set_{train_size}/{case}/train_boundary.png')
         plt.show()
+
+        plot_decision_boundary(lambda x: classifier.predict(x),
+                               X_test, y_test, title=f'Decision boundary for {case} epoch on test set')
+        plt.savefig(f'exp_4/set_{train_size}/{case}/test_boundary.png')
+        plt.show()
+
+
+def test_different_starting_configurations(dataset, train_size):
+    X_train, X_test, y_train, y_test = train_test_split(dataset[:, :-1],
+                                                        dataset[:, -1],
+                                                        train_size=train_size,
+                                                        test_size=0.2,
+                                                        random_state=42)
+
+    all_classes = np.unique(dataset[:, -1])
+    epochs = range(1, NUM_EPOCHS + 1)
+
+    random_seeds = [12, 31, 16, 2353, 314, 6523, 634, 23, 802, 134]
+
+    print(f'- Training set size: {train_size}')
+    print('Seed, '
+          'train_accuracy_first, train_accuracy_best, train_best_epoch, train_accuracy_end, '
+          'test_accuracy_first, test_accuracy_best, test_best_epoch, test_accuracy_end')
+    for seed in random_seeds:
+        classifier = MLPClassifier(hidden_layer_sizes=(5,),  # changed to the most optimal num_neurons from exp 2 and 3
+                                   activation='relu',
+                                   max_iter=NUM_EPOCHS,
+                                   tol=0,
+                                   n_iter_no_change=100000,
+                                   solver='sgd',
+                                   random_state=seed)
+
+        train_scores = []
+        test_scores = []
+
+        for _ in epochs:
+            classifier.partial_fit(X_train, y_train, classes=all_classes)
+
+            train_score = classifier.score(X_train, y_train)
+            test_score = classifier.score(X_test, y_test)
+
+            train_scores.append(train_score)
+            test_scores.append(test_score)
+
+        train_accuracy_first = train_scores[0]
+        train_accuracy_best = np.max(train_scores)
+        train_best_epoch = np.argmax(train_scores) + 1
+        train_accuracy_end = train_scores[-1]
+
+        test_accuracy_first = test_scores[0]
+        test_accuracy_best = np.max(test_scores)
+        test_best_epoch = np.argmax(test_scores) + 1
+        test_accuracy_end = test_scores[-1]
+
+        print(f'{seed}, '
+              f'{train_accuracy_first}, {train_accuracy_best}, {train_best_epoch}, {train_accuracy_end}, '
+              f'{test_accuracy_first}, {test_accuracy_best}, {test_best_epoch}, {test_accuracy_end}')
 
 
 if __name__ == '__main__':
     for dataset, name in load_generated_datasets()[5:6]:
         # For both train sizes
         for train_size in [0.2, 0.8]:
-            experiment_4(dataset, name, train_size)
+            print(f'Starting experiment with train set size of {train_size}')
+            visualize_training_over_time(dataset, train_size)
+            test_different_starting_configurations(dataset, train_size)
