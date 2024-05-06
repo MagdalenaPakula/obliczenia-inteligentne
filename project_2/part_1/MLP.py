2ab71218831d29d7be088a273664084e75255e4e
MagdalenaPakula <magdalena.pakula001@gmail.com>
2024-05-06 22:07:41 +0200

    MLP works on test dataset fromm wine, breast and cancer. Created latex file ;)

--- project_2/part_1/MLP.py
+++ project_2/part_1/MLP.py
@@ -1,58 +1,101 @@
 import torch
 import torch.nn as nn
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
 from torch.utils.data import DataLoader
 from project_2.part_1.data.MNIST import load_dataset_MNIST
 from project_2.part_1.data.other_datasets import load_other_datasets, CustomDataset
+import torch.optim as optim
 
 
+# Definicja klasy MLP
 class MLP(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, use_relu=True):
         super(MLP, self).__init__()
 
-        # Define layers
         self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.relu = nn.ReLU(inplace=True) if use_relu else None  # Optional ReLU activation
+        self.relu = nn.ReLU(inplace=True) if use_relu else None
         self.fc2 = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
         # Forward pass through layers
         x = self.fc1(x)
-        if self.relu:
-            x = self.relu(x)
+        x = self.relu(x)
         x = self.fc2(x)
         return x
 
 
-def train_model(model, train_loader, test_loader, learning_rate=0.01, num_epochs=10):
-
+# Funkcja trenująca model
+def train_model(model, train_loader, device, epochs):
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
 
-    # Training loop
-    for epoch in range(num_epochs):
-        print(f"Epoch: {epoch + 1}/{num_epochs}")
+    model.to(device)
 
-        # Train phase
-        model.train()
-        for data, target in train_loader:
+    for epoch in range(epochs):
+        print(f"Epoka {epoch + 1}/{epochs}")
+        running_loss = 0.0
+        for i, (data, labels) in enumerate(train_loader):
+            data = data.to(device)
+            labels = labels.long().to(device)  # Convert labels to torch.long
+
             optimizer.zero_grad()
-            output = model(data)
-            loss = criterion(output, target)
+            outputs = model(data.float())
+            loss = criterion(outputs, labels)
+
             loss.backward()
             optimizer.step()
 
-        # Optional evaluation
-        if test_loader is not None:
-            with torch.no_grad():
-                correct = 0
-                total = 0
-                model.eval()
-                for data, target in test_loader:
-                    output = model(data)
-                    _, predicted = torch.max(output.data, 1)
-                    total += target.size(0)
-                    correct += (predicted == target).sum().item()
-                accuracy = 100 * correct / total
-                print(f"Test Accuracy: {accuracy:.2f}%")
+            running_loss += loss.item()
 
+            if i % 100 == 99:
+                print(f"[Step {i + 1}/{len(train_loader)}] Loss: {running_loss / 100}")
+                running_loss = 0.0
+
+
+
+# Funkcja ewaluująca model
+def evaluate_model(model, test_loader, device):
+    model.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for data, labels in test_loader:
+            data = data.to(device)
+            labels = labels.to(device)
+            outputs = model(data.float())
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(f'Dokładność na zbiorze testowym: {correct / total * 100}%')
+
+
+if __name__ == "__main__":
+    input_dim = [4, 13, 30]  # Input dimensions for: Iris, Wine & Breast Cancer
+    hidden_dim = 16
+    epochs = 10
+
+    datasets = load_other_datasets()
+
+    for dataset in datasets:
+        train_dataset = dataset['train_dataset']
+        test_dataset = dataset['test_dataset']
+
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+        # Extract target labels from training dataset
+        target_labels = []
+        for _, label in train_dataset:
+            target_labels.append(label.item())
+
+        # Convert to set and calculate number of classeschat
+        num_classes = len(set(target_labels))
+
+        # Define the model
+        model = MLP(input_dim[datasets.index(dataset)], hidden_dim, num_classes)
+
+        train_model(model, train_loader, 'cpu', epochs)
+        evaluate_model(model, test_loader, 'cpu')
 
