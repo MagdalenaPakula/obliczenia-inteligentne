22190367bea1431f587b629fab4cd20dfb2e87bc
Jakub Pawlak <kuba_pawlak@icloud.com>
2024-05-13 20:30:17 +0200

    Added visualization of decision boundary to t-SNE

--- project_1/visualization.py
+++ project_1/visualization.py
@@ -103,6 +103,7 @@ def plot_decision_boundary(classifier: Callable[[ndarray], ndarray],
                            title: Optional[str] = None,
                            resolution: int = 100,
                            cmap=plt.cm.RdYlBu,
+                           size=50
                            ) -> None:
     if features.shape[1] != 2:
         raise ValueError("Plotting decision boundary requires 2D features")
@@ -111,12 +112,14 @@ def plot_decision_boundary(classifier: Callable[[ndarray], ndarray],
     x_min, x_max = features[:, 0].min() - 1, features[:, 0].max() + 1
     y_min, y_max = features[:, 1].min() - 1, features[:, 1].max() + 1
     xx, yy = np.meshgrid(np.linspace(x_min, x_max, resolution), np.linspace(y_min, y_max, resolution))
+    xx = xx.astype(np.float32)
+    yy = yy.astype(np.float32)
 
     # Predict labels for each point in the mesh
     y_pred = classifier(np.c_[xx.ravel(), yy.ravel()])
     y_pred = y_pred.reshape(xx.shape)
-    plt.contourf(xx, yy, y_pred, alpha=0.5, cmap=cmap)
-    plt.scatter(features[:, 0], features[:, 1], c=labels, cmap=cmap)
+    plt.contourf(xx, yy, y_pred, alpha=0.2, cmap=cmap)
+    plt.scatter(features[:, 0], features[:, 1], c=labels, cmap=cmap, s=size)
 
     if title:
         plt.title(title)
--- project_2/part_1/experiments/mnist_tsne.py
+++ project_2/part_1/experiments/mnist_tsne.py
@@ -1,17 +1,79 @@
+import numpy as np
 import torch
+from matplotlib import pyplot as plt
+from torch.utils.data import DataLoader
 
+from project_1.visualization import plot_decision_boundary
 from project_2.part_1.MLP import MLP
-from project_2.part_1.experiments import perform_experiment
+from project_2.part_1.experiments import train_model, evaluate_model
 from project_2.part_1.features.two_dimentional.tsne import get_mnist_tsne
 
-if __name__ == '__main__':
-    dataset = get_mnist_tsne()
+MODEL_PATH = "mnist_tsne_model.pt"
 
+
+def get_prediction(model, x: np.ndarray) -> np.ndarray:
+    num_predictions = x.shape[0]
+    with torch.inference_mode():
+        result = model(torch.tensor(x))
+        result = result.numpy().astype(np.float32)
+        assert result.shape[0] == num_predictions
+        return result.argmax(axis=1)
+
+
+def create_and_train_model(train_loader) -> torch.nn.Module:
     torch.manual_seed(42)
     input_size = 2
     hidden_layer_size = 4
     output_size = 10
-
     model = MLP(input_size, hidden_layer_size, output_size)
+    train_model(model, train_loader, epochs=100, learning_rate=0.05)
+    torch.save(model, MODEL_PATH)
+    return model
 
-    perform_experiment(dataset, model, epochs=150, learning_rate=0.05)
+
+def loader_to_np_array(loader: DataLoader) -> tuple[np.ndarray, np.ndarray]:
+    features, labels = np.zeros((0, 2)), np.zeros((0,))
+    for data, l in loader:
+        features = np.append(features, data, axis=0)
+        labels = np.append(labels, l, axis=0)
+
+    return np.array(features), np.array(labels)
+
+
+def main():
+    dataset = get_mnist_tsne()
+
+    name = dataset['name']
+    train_dataset = dataset['train_dataset']
+    test_dataset = dataset['test_dataset']
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+    # get model
+    try:
+        model = torch.load(MODEL_PATH)
+    except FileNotFoundError:
+        model = create_and_train_model(train_loader)
+
+    evaluate_model(model, test_loader, name)
+
+    x_test, y_fact = loader_to_np_array(test_loader)
+
+    x_test = x_test.astype(np.float32)[:1000]
+    y_fact = y_fact.astype(np.float32)[:1000]
+
+    y_pred = get_prediction(model, x_test)
+
+    def classifier(x: np.ndarray) -> np.ndarray:
+        return get_prediction(model, x)
+
+    plot_decision_boundary(classifier, x_test, y_fact, cmap='tab10', resolution=500, size=10,
+                           title='MNIST decision boundary with real labels')
+    plt.show()
+    plot_decision_boundary(classifier, x_test, y_pred, cmap='tab10', resolution=500, size=10,
+                           title='MNIST decision boundary with predicted labels')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
