2e7a53c7f7047e76493d8466ff248fb76b7bf567
Jakub Pawlak <kuba_pawlak@icloud.com>
2024-05-21 18:28:16 +0200

    Cleaned up validation dataloaders

--- project_2/part_2/data/__init__.py
+++ project_2/part_2/data/__init__.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import pytorch_lightning as pl
 import torchvision.transforms as transforms
+from pytorch_lightning.utilities.types import EVAL_DATALOADERS
 from torch.utils.data import Dataset, DataLoader
 from torchvision.datasets import MNIST, CIFAR10
 
@@ -14,17 +15,18 @@ class MNISTDataModule(pl.LightningDataModule):
         self.batch_size = 100
         self.data_dir = DATA_DIR
         self.transform = transforms.Compose([transforms.ToTensor()])
+        self.train_dataset: Dataset | None = None
+        self.test_dataset: Dataset | None = None
 
     def setup(self, stage: str) -> None:
         self.train_dataset: Dataset = MNIST(self.data_dir, train=True, transform=self.transform, download=True)
         self.test_dataset: Dataset = MNIST(self.data_dir, train=False, transform=self.transform, download=True)
-        self.val_dataset: Dataset = MNIST(self.data_dir, train=False, transform=self.transform, download=True)
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=4, shuffle=True)
 
     def val_dataloader(self) -> DataLoader:
-        return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=4)
+        return self.test_dataloader()
 
     def test_dataloader(self) -> DataLoader:
         return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=4)
@@ -36,6 +38,8 @@ class CIFAR10DataModule(pl.LightningDataModule):
         self.batch_size = 64
         self.data_dir = DATA_DIR
         self.transform = transforms.Compose([transforms.ToTensor()])
+        self.train_dataset: Dataset | None = None
+        self.test_dataset: Dataset | None = None
 
     def setup(self, stage: str) -> None:
         self.train_dataset: Dataset = CIFAR10(self.data_dir, train=True, download=True)
@@ -43,6 +47,9 @@ class CIFAR10DataModule(pl.LightningDataModule):
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(self.train_dataset, num_workers=4, batch_size=self.batch_size, shuffle=True)
+
+    def val_dataloader(self) -> EVAL_DATALOADERS:
+        return self.test_dataloader()
 
     def test_dataloader(self) -> DataLoader:
         return DataLoader(self.test_dataset, batch_size=self.batch_size)
