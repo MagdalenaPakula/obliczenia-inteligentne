60cc9b8aa572a160af662b8b710f48da806919ab
Jakub Pawlak <kuba_pawlak@icloud.com>
2024-05-27 18:08:21 +0200

    Added experiment 2 for case with no augmentation

---
+++ project_2/part_2/experiment_2/no_augmentation/cifar_small.py
@@ -0,0 +1,27 @@
+import pytorch_lightning as pl
+from torchvision.transforms import v2
+
+from project_2.part_2.data.augmented import AugmentedCIFAR10DataModule
+from project_2.part_2.experiment_2 import perform_experiment_2
+from project_2.part_2.models.magda.cifar.small import MagdaCifarSmall
+
+
+def _main():
+    augmentation = v2.Identity()
+
+    def create_module(subset_size: int) -> pl.LightningDataModule:
+        return AugmentedCIFAR10DataModule(
+            transform=v2.Compose([v2.ToTensor()]),
+            augmentation=augmentation,
+            subset_size=subset_size,
+            num_augmentations=1,
+        )
+
+    def create_model() -> pl.LightningModule:
+        return MagdaCifarSmall(num_classes=10)
+
+    perform_experiment_2(create_module, create_model, epochs=4)
+
+
+if __name__ == '__main__':
+    _main()
