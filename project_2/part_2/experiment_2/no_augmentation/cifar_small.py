0cb415035452f7a77365dfd904f2b97b4a2ce8ac
MagdalenaPakula <magdalena.pakula001@gmail.com>
2024-05-29 11:10:57 +0200

    Repo update - FINAL

--- project_2/part_2/experiment_2/no_augmentation/cifar_small.py
+++ project_2/part_2/experiment_2/no_augmentation/cifar_small.py
@@ -1,6 +1,7 @@
 import pytorch_lightning as pl
 from torchvision.transforms import v2
 
+from project_2.part_2.data import CIFAR10DataModule
 from project_2.part_2.data.augmented import AugmentedCIFAR10DataModule
 from project_2.part_2.experiment_2 import perform_experiment_2
 from project_2.part_2.models.magda.cifar.small import MagdaCifarSmall
@@ -10,11 +11,10 @@ def _main():
     augmentation = v2.Identity()
 
     def create_module(subset_size: int) -> pl.LightningDataModule:
-        return AugmentedCIFAR10DataModule(
-            transform=v2.Compose([v2.ToTensor()]),
-            augmentation=augmentation,
-            subset_size=subset_size,
-            num_augmentations=1,
+        return CIFAR10DataModule(
+            transform=v2.Compose([
+                v2.ToTensor(),
+                augmentation]),
         )
 
     def create_model() -> pl.LightningModule:
