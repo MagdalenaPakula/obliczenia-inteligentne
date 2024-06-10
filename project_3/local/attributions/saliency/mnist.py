3f00f3d934071e2dd1b88aa8d849078df8327d18
Jakub Pawlak <kuba_pawlak@icloud.com>
2024-06-10 23:04:01 +0200

    Add saliency on MNIST dataset

---
+++ project_3/local/attributions/saliency/mnist.py
@@ -0,0 +1,51 @@
+from typing import Tuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from captum.attr import Saliency
+
+from project_2.part_2.data import MNISTDataModule
+from project_2.part_2.models.kuba import MnistLargeModel
+from project_3.models import get_model
+
+
+def get_sample_data(index: int = 0) -> Tuple[torch.Tensor, torch.Tensor]:
+    dm = MNISTDataModule()
+    dm.setup('')
+    dataloader = dm.test_dataloader()
+
+    images, labels = next(iter(dataloader))
+
+    return images[index], labels[index]
+
+
+def plot_original_and_attributions(original: torch.Tensor, attributions: torch.Tensor):
+    original_img = original.permute(1, 2, 0).detach().numpy()
+    attributions_img = attributions.permute(1, 2, 0).detach().numpy()
+
+    fig, axis = plt.subplots(1, 2)
+    axis[0].imshow(original_img, cmap='gray')
+    axis[0].set_title('Original image')
+    axis[1].imshow(attributions_img)
+    axis[1].set_title('Attributions')
+    fig.show()
+
+
+def _main():
+    model: MnistLargeModel = get_model("kuba_mnist_big.pt")
+
+    for i in range(10):
+        img, label = get_sample_data(i)
+        img.requires_grad = True
+
+        saliency = Saliency(model)
+        model.eval()
+        model.zero_grad()
+        gradients = saliency.attribute(img.unsqueeze(0), target=label.item())
+
+        plot_original_and_attributions(img, gradients.squeeze(0))
+
+
+if __name__ == '__main__':
+    _main()
