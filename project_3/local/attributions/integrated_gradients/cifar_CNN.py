ce59d0f5fc6115371dbb66e64b652bbe08eb98b4
MagdalenaPakula <magdalena.pakula001@gmail.com>
2024-06-11 18:00:13 +0200

    integrated gradients on cifar CNN :)

--- project_3/local/attributions/integrated_gradients/cifar_CNN.py
+++ project_3/local/attributions/integrated_gradients/cifar_CNN.py
@@ -5,10 +5,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from captum.attr import visualization as viz
+from matplotlib.colors import LinearSegmentedColormap
 
 from project_2.part_2.data import CIFAR10DataModule
 from project_2.part_2.models.kuba.cifar.large import CifarLargeModel, _CIFARLargeFeatureExtractor
 from project_3.models import get_model
+
 
 
 def get_sample_data_cifar(index: int = 0) -> Tuple[torch.Tensor, torch.Tensor]:
@@ -36,9 +38,10 @@ def plot_original_and_attributions_with_blended_heatmap(original: torch.Tensor, 
     ax1.set_title('Original image')
 
     # Visualize the blended heatmap - sign="all", sign="positive", sign="negative"
-    viz.visualize_image_attr(attributions_img, original_image=original_img, method="blended_heat_map", sign="all",
+    viz.visualize_image_attr(attributions_img, original_image=original_img, method='blended_heat_map', sign="positive",
                              show_colorbar=True, title="Overlayed Integrated Gradients", plt_fig_axis=(fig, ax2))
-
+    # outlier_perc=1
+    # method = "blended_heat_map" or  "heat_map"
     fig.tight_layout()
     plt.show()
 
