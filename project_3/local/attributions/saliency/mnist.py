b0e676539dbf96b4467533241de8cbd2add03191
Jakub Pawlak <kuba_pawlak@icloud.com>
2024-06-11 18:34:00 +0200

    Modify saliency visualization

--- project_3/local/attributions/saliency/mnist.py
+++ project_3/local/attributions/saliency/mnist.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from captum.attr import Saliency
+from captum.attr import visualization as viz
 
 from project_2.part_2.data import MNISTDataModule
 from project_2.part_2.models.kuba import MnistLargeModel
@@ -13,7 +14,7 @@ from project_3.models import get_model
 def get_sample_data(index: int = 0) -> Tuple[torch.Tensor, torch.Tensor]:
     dm = MNISTDataModule()
     dm.setup('')
-    dataloader = dm.test_dataloader()
+    dataloader = dm.train_dataloader()
 
     images, labels = next(iter(dataloader))
 
@@ -31,9 +32,12 @@ def plot_original_and_attributions(original: torch.Tensor, attributions: torch.T
     axis[0].imshow(original_img, cmap='gray')
     axis[0].set_axis_off()
     axis[0].set_title('Original image')
-    axis[1].imshow(attributions_img)
-    axis[1].set_axis_off()
-    axis[1].set_title('Attributions')
+    viz.visualize_image_attr(attributions_img, original_img,
+                             method='blended_heat_map',
+                             sign='absolute_value',
+                             plt_fig_axis=(fig, axis[1]),
+                             use_pyplot=False)
+    axis[1].set_title('Overlayed attributions')
     fig.show()
 
 
@@ -41,7 +45,7 @@ def _main():
     model: MnistLargeModel = get_model("kuba_mnist_big.pt")
 
     for i in range(10):
-        img, label = get_sample_data(i)
+        img, label = get_sample_data(i + 10)
         img.requires_grad = True
 
         saliency = Saliency(model)
