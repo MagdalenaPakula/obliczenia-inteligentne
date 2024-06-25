98562e31bae450afe2795d2f0feb0e3ca2de37da
magda <magdalena.pakula001@gmail.com>
2024-06-25 18:03:01 +0200

    lbp alone + org

--- project_3/local/attributions/saliency/lbp.py
+++ project_3/local/attributions/saliency/lbp.py
@@ -13,6 +13,10 @@ def get_transformed_data(index: int = 0) -> Tuple[torch.Tensor, torch.Tensor]:
     dataset = load_dataset_MNIST(transform=lbp_feature_extraction)['test_dataset']
     return dataset[index]
 
+def get_original_data(index: int = 0) -> Tuple[torch.Tensor, torch.Tensor]:
+    dataset = load_dataset_MNIST()['test_dataset']
+    return dataset[index]
+
 
 def lbp_feature_extraction(image):
     image = image.squeeze().numpy()
@@ -21,13 +25,26 @@ def lbp_feature_extraction(image):
 
 
 def _main():
-    image, label = get_transformed_data(index=0)
+    # Get the original and LBP-transformed images
+    original_image, label = get_original_data(index=0)
+    lbp_image, label = get_transformed_data(index=0)
+
+    # Create a figure with two subplots
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+
+    # Plot the original image
+    ax1.imshow(original_image.squeeze(), cmap='gray')
+    ax1.set_title(f'Original Image - Label: {label}')
+    ax1.set_xticks(np.arange(0, 28, step=4))
+    ax1.set_yticks(np.arange(0, 28, step=4))
+    ax1.grid(True, color='gray', linestyle='--', linewidth=0.5)
 
     # Plot the LBP image
-    plt.figure(figsize=(5, 5))
-    plt.imshow(image, cmap='gray')
-    plt.title(f'LBP Image - Label: {label}')
+    ax2.imshow(lbp_image, cmap='gray')
+    ax2.set_title(f'LBP Image - Label: {label}')
+
     plt.show()
+
 
 if __name__ == '__main__':
     _main()
